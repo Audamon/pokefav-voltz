c3d79b43e46f69f8fa0dc300088e702057d900f9
Audamon <joaotamagno@gmail.com>
2022-04-04 21:33:26 -0300

    Header was implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import  HeaderContainer from './components/Header/Header';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <HeaderContainer></HeaderContainer>
+    <PageContainer>
+  
+    </PageContainer>
+    </>
   );
 }
 
 export default App;
+
+
+const PageContainer = styled.div`
+  background-image: url('https://images7.alphacoders.com/662/thumb-1920-662102.png');
+  height: 100%;
+  width: 100%;
+  display: flex;
+`;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components';
+import logo from '../../shared/namelogo.png';
+
+function Header() {
+  return (
+    <HeaderContainer>
+        <LogoContainer>
+            <img src={logo} alt="logo" />
+        </LogoContainer>
+
+    </HeaderContainer>
+  )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+    background-color: #ef5350;
+    height: 80px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LogoContainer = styled.div`
+    > img{
+        height: 50px;
+    }
+
+`;
